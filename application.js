ae9884f4d45d50ff55619af3bd32c598d5984b38
Srey2255 <SReyes@kippnyccp.org>
2017-06-08 11:34:20 -0400

    Create application.js

--- application.js
+++ application.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  $("body").click(function(){
-  alert("Dwane Is Trash"):
+  $("p").click(function(){
+    alert("Dwane Is Trash"):
+    $(this).css("color","red");
+    $("#h1").html("hi, how are you? My name is Sam and I am better than Dwane,the guy next to me in Computer Science class. ");
+  });
+  
+  $("h1").click(function(){
+    alert("Although he may not think I am better than him, I am"):
   });
 });
+
